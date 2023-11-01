1257f08c6639d48c46eba63cadd9ee0ed2dee1a4
1Max58 <>
2023-11-01 20:18:25 +0200

    Initial commit

---
+++ src/com/example/carinformation/Car.java
@@ -0,0 +1,19 @@
+package com.example.carinformation;
+
+public class Car {
+    private String model;
+    private String brand;
+    private int year;
+    private float price;
+    private String color;
+    private int quantity;
+
+    public Car(String m, String b, int y, float p, String c, int q) {
+        model = m;
+        brand = b;
+        year = y;
+        price = p;
+        color = c;
+        quantity = q;
+    }
+}
